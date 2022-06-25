667d60582ce48539bff0e90826c656c391a82e91
xuanbinh2721 <binhphan2721@gmail.com>
2022-06-25 22:04:16 +0700

    update setting

--- app/src/main/java/com/example/clothingshop/ConfirmFinalOrder.java
+++ app/src/main/java/com/example/clothingshop/ConfirmFinalOrder.java
@@ -12,5 +12,6 @@ public class ConfirmFinalOrder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_final_order);
+
     }
 }
--- app/src/main/java/com/example/clothingshop/setting.java
+++ app/src/main/java/com/example/clothingshop/setting.java
@@ -1,13 +1,9 @@
 package com.example.clothingshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +11,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clothingshop.Prevalent.Prevalent;
 import com.google.android.gms.tasks.Continuation;
@@ -29,6 +28,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.util.HashMap;
 
@@ -40,7 +40,7 @@ public class setting extends AppCompatActivity {
     private EditText edtChangeName, edtChangePhone, edtChangeAddress;
     // SETTING BUTTONS
     private TextView profileChangeTextBtn,  closeTextBtn, saveTextButton;
-    private Button btnUpdate;
+    private Button btnUpdate, btnClose;
     // TO GET THE PICTURE STORED IN FIREBASE STORAGE  ALREADY
     private Uri uri;
     private ImageView profilePic;
@@ -51,7 +51,7 @@ public class setting extends AppCompatActivity {
     private String myUrl = "";
     private StorageReference storageProfilePictureRef;
     private String checker = "";
-
+    private StorageTask uploadTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,7 +63,8 @@ public class setting extends AppCompatActivity {
         edtChangeAddress = (EditText) findViewById(R.id.edtchangeAddress);
         profileChangeTextBtn = (TextView)findViewById(R.id.change_avt);
         profileImageView = (ImageView) findViewById(R.id.imageView2);
-
+        btnClose = (Button) findViewById(R.id.btnClose);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
         // CONSTRUCTOR TO CREATE USER DATABASE USING USER DATABASE HELPER CLASS
         userDb = new DatabaseHelperCustomer(this);
 
@@ -71,34 +72,34 @@ public class setting extends AppCompatActivity {
         userInfoDisplay(edtChangeName,edtChangePhone,edtChangeAddress);
 
         // CLOSE BUTTON
-//        closeTextBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view){
-//                // TO END THE CURRENT TASK AND GO BACK TO THE PREVIOUS SCREEN
-//                finish();
-//            }
-//        });
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                // TO END THE CURRENT TASK AND GO BACK TO THE PREVIOUS SCREEN
+                finish();
+            }
+        });
         //  BUTTON TO CHANGE THE PROFILE PICTURE
         profileChangeTextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-//                checker = "clicked";
-//                CropImage.activity(imageUri)
-//                        .setAspectRatio(1, 1)
-//                        .start(setting.this);
+                checker = "clicked";
+                CropImage.activity(imageUri)
+                        .setAspectRatio(1, 1)
+                        .start(setting.this);
             }
         });
-//        btnUpdate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view){
-//                if (checker.equals("clicked")){
-//                    userInfoSaved();
-//                }
-//                else{
-//                    updateUserInfo();
-//                }
-//            }
-//        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if (checker.equals("clicked")){
+                    userInfoSaved();
+                }
+                else{
+                    updateUserInfo();
+                }
+            }
+        });
     }
     private void updateUserInfo(){
         String oldphone = Prevalent.currentOnlineUser.getPhone();
@@ -112,16 +113,16 @@ public class setting extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE  &&  resultCode==RESULT_OK  &&  data!=null){
-//            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-//            imageUri = result.getUri();
-//            profileImageView.setImageURI(imageUri);
-//        }
-//        else{
-//            Toast.makeText(this, "Error, Try Again.", Toast.LENGTH_SHORT).show();
-//            startActivity(new Intent(setting.this, setting.class));
-//            finish();
-//        }
+        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE  &&  resultCode==RESULT_OK  &&  data!=null){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+            profileImageView.setImageURI(imageUri);
+        }
+        else{
+            Toast.makeText(this, "Error, Try Again.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(setting.this, setting.class));
+            finish();
+        }
     }
     private void userInfoSaved(){
         if (TextUtils.isEmpty(edtChangeName.getText().toString())){
@@ -137,46 +138,46 @@ public class setting extends AppCompatActivity {
 //            uploadImage();
         }
     }
-//    private void uploadImage(){
-//
-//        if (imageUri != null){
-//            final StorageReference fileRef = storageProfilePictureRef.child(Prevalent.currentOnlineUser.getPhone() + ".jpg");
-//            uploadTask = fileRef.putFile(imageUri);
-//            uploadTask.continueWithTask(new Continuation() {
-//                @Override
-//                public Object then(@NonNull Task task) throws Exception{
-//                    if (!task.isSuccessful()){
-//                        throw task.getException();
-//                    }
-//                    return fileRef.getDownloadUrl();
-//                }
-//            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-//                @Override
-//                public void onComplete(@NonNull Task<Uri> task){
-//                    if (task.isSuccessful()){
-//                        Uri downloadUrl = task.getResult();
-//                        myUrl = downloadUrl.toString();
-//                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
-//                        HashMap<String, Object> userMap = new HashMap<>();
-//                        userMap. put("image", myUrl);
-//                        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
-//                        Prevalent.currentOnlineUser.setProfilePic("true");
-//                        startActivity(new Intent(setting.this, HomePage.class));
-//                        Toast.makeText(setting.this, "Profile Info updated successfully!", Toast.LENGTH_SHORT).show();
-//                        finish();
-//                    }
-//                    else
-//                    {
-//                        Toast.makeText(setting.this, "Error.", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//        }
-//        else
-//        {
-//            Toast.makeText(this, "Image is not selected.", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+    private void uploadImage(){
+
+        if (imageUri != null){
+            final StorageReference fileRef = storageProfilePictureRef.child(Prevalent.currentOnlineUser.getPhone() + ".jpg");
+            uploadTask = fileRef.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception{
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task){
+                    if (task.isSuccessful()){
+                        Uri downloadUrl = task.getResult();
+                        myUrl = downloadUrl.toString();
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap. put("image", myUrl);
+                        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
+                        Prevalent.currentOnlineUser.setProfilePic("true");
+                        startActivity(new Intent(setting.this, HomePage.class));
+                        Toast.makeText(setting.this, "Profile Info updated successfully!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    else
+                    {
+                        Toast.makeText(setting.this, "Error.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(this, "Image is not selected.", Toast.LENGTH_SHORT).show();
+        }
+    }
     private void userInfoDisplay(final EditText edtChangeName,final EditText edtChangePhone,final EditText edtChangeAddress){
         // RETRIEVING USER DATA FROM SQLITE DATABASE
         String phone = Prevalent.currentOnlineUser.getPhone();
