535e3989c07092d7a7ae49ffeb54b96e55d4d959
NDHuynhDN <duchuynh3011@gmail.com>
2022-06-25 15:18:11 +0700

    sua loi

--- app/src/main/java/com/example/clothingshop/HomePage.java
+++ app/src/main/java/com/example/clothingshop/HomePage.java
@@ -40,7 +40,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
     private DatabaseReference ProductsRef;
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
-    TextView textNameLogin;
+    TextView useNameTextView;
 
     private String userType = "null";
 
@@ -49,45 +49,35 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        recyclerView = findViewById(R.id.product_content);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         if (bundle != null) {
             // this will work only if we come from the admin activity
-            userType = getIntent().getExtras().get("Admin").toString();
+            userType = getIntent().getExtras().get("Owner").toString();
         }
         ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         // add toolbar
-        if (!userType.equals("Admin")) {
+        if (!userType.equals("Owner")) {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
             DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
             ActionBarDrawerToggle toggle =
-                    new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_drawer_open, R.string.nav_drawer_close);
+                    new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
             drawerLayout.addDrawerListener(toggle);
             toggle.syncState();
 
             NavigationView navigationView = findViewById(R.id.navigation_view);
             navigationView.setNavigationItemSelectedListener(this);
 
-            findViewById(R.id.btnMenu).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    drawerLayout.openDrawer(GravityCompat.START);
-                }
-            });
             View headerView = navigationView.getHeaderView(0);
             TextView useNameTextView = headerView.findViewById(R.id.textName);
             ImageView profileImageView = headerView.findViewById(R.id.imageView);
-
-
             useNameTextView.setText(currentOnlineUser.getName());
+            //useNameTextView.setText(currentOnlineUser.getName());
             //Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
             DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentOnlineUser.getPhone());
             UsersRef.addValueEventListener(new ValueEventListener() {
@@ -107,6 +97,10 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
                 }
             });
         }
+        recyclerView = findViewById(R.id.product_content);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
     protected void onStart(){
@@ -156,35 +150,33 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_home){
-//            if(currnentFrag != FRAGMENT_HOME){
-//                replaceFragment(new content_home_fragment());
-//                currnentFrag = FRAGMENT_HOME;
-//
 
+        if (id == R.id.nav_home){
+            if (!userType.equals("Owner")) {
+                Intent intent = new Intent(HomePage.this, HomePage.class);
+                startActivity(intent);
+            }
         } else if (id == R.id.nav_car){
-            Intent intent = new Intent(HomePage.this, ContentCart.class);
-            startActivity(intent);
-//            if(currnentFrag != FRAGMENT_CART){
-//                replaceFragment(new content_cart_fragment());
-//                currnentFrag = FRAGMENT_CART;
-//            }
+            if (!userType.equals("Owner")) {
+                Intent intent = new Intent(HomePage.this, ContentCart.class);
+                startActivity(intent);
+            }
         } else if (id == R.id.nav_setting) {
-//            if (currnentFrag != FRAGMENT_SETTING) {
-//                replaceFragment(new setting_fragment());
-//                currnentFrag = FRAGMENT_SETTING;
-//            }
-            Intent intent = new Intent(HomePage.this, setting.class);
-            startActivity(intent);
+            if (!userType.equals("Owner")) {
+                Intent intent = new Intent(HomePage.this, setting.class);
+                startActivity(intent);
+            }
         }
         else if (id == R.id.nav_logout ) {
            finish();
         }
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
     // xu ly khi bam nut back cua thiet bi
     public void onBackPressed(){
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
